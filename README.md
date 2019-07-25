89e06bbd554ef92a4f32ae603c68c619e6132875
bkimmett <bkimmett@users.noreply.github.com>
2019-07-25 01:19:31 -0700

    Version 1.1, now with command line options and better help messages!

--- README.md
+++ README.md
@@ -26,8 +26,28 @@ The basic idea is just run the script and feed it names of files to upload, such
 
 It'll upload those files, or tell you what went wrong.
 
-\
-You can change what gallery your files are uploaded to by changing the value of the 'gallery' variable in the script. The name should match the name of one of your existing LPix galleries.
+## Options: Image URL Logging, Custom Galleries
+
+Megasend has three command-line options. They are --log, --tlog, and --gallery.
+
+--log and --tlog will write the image URLs of the uploaded images to a log file you specify, with \[img\]\[/img\] tags (when using --log) or \[timg\]\[/timg\] tags (when using --tlog) around them. This can be handy for batch-replacing the URLs into a post later.
+
+If you specify the same file for both --log and --tlog, each file's \[img\] and \[timg\] links are kept together.
+
+`./megasend.py --log image_urls.txt FileBatch*.png`
+
+`./megasend.py --tlog thumb_urls.txt HowToMakeOneMillionImagesInThirtyDays*.png`
+
+`./megasend.py --log image_urls.txt --tlog thumb_urls.txt upload_this.png`
+
+You can use --gallery to change what gallery your files are uploaded to, if given the name of the gallery (in quotes).
+
+If the gallery of the name you specified doesn't exist, I have no idea what will happen. Please double-check your gallery names before uploading.
+
+`./megasend.py --gallery "Dubstep Visualizations" Bass_Drop.png`
+
+
+## Boosted Upload Limit
 
 If you have a boosted upload limit, you can also change the script's 'my\_mb\_limit' variable to let Megasend know - normally it's set to 2, for a 2MB upload limit, but you can change it to the size of your personal limit.  
   
@@ -38,11 +58,9 @@ Here are some ideas for improvements, which I haven't yet got around to making.
 
 (If you decide to implement one of these, please test your changes on Python 2 _and_ 3 to make sure they both work!)
 
-* *Command-line arguments (gallery):* Specifying a destination gallery name through a command-line argument. As it is now, it's kind of hard to change and easy to forget that you have changed it.
-
 * *Username/password storage outside script:* Currently, sharing your personal copy of this script is a bad idea. Outside-of-script user/pass storage would fix that.
 
-* *Folder support:* Currently, feeding Megasend a folder name will make it try to open it like a file, and fail.
+* *Folder support:* Currently, feeding Megasend a folder name will make it politely reject it, which is an improvement from last version - but recursively uploading its contents might be better.
 
 
 ['requests']: https://2.python-requests.org/en/master/user/install/#install
--- megasend.py
+++ megasend.py
@@ -1,35 +1,81 @@
 #!/usr/bin/env python
 from __future__ import print_function
-from sys import argv
-from os.path import getsize
+from sys import argv, exit
+from os.path import basename, getsize, isfile, isdir
+import argparse
 import requests #error? try 'pip install requests'
-
 
 #HEY YOU: Change these to your personal LPix username and password.
 username = 'AzureDiamond'
 password = 'hunter2'
-
-gallery = 'Default' #you can change this to upload into another gallery, too.	
-
 my_mb_limit = 2 #Most LPix users have a 2 MB upload limit. If you have a boosted upload limit, increase this number to your personal limit, in MB.
 
+def print_usage():
+	print("usage: {} [--help] [--log <log_file>] [--tlog <log_file>] [--gallery <gallery_name>] file1 [file2] [file3...]".format(basename(argv[0])))
+	
+def print_help():
+	print_usage()
+	print("\n--log and --tlog take the name of a log file to store all resulting image URLs. The --log file saves [img] codes, and the --tlog file saves [timg] codes.\n--gallery takes the name of the gallery to upload to. If you don't specify, 'Default' is used.\n")
+	
 
-if len(argv) < 2:
-	print("Usage: {} file1 [file2] [file3...]".format(argv[0]))
+if len(argv) < 2: #no arguments at ALL? display help message
+	print_help()
+	exit()
 
-for file in argv[1:]:
+#parse arguments
+parser = argparse.ArgumentParser(usage="%(prog)s [--help] [--log <log_file>] [--tlog <log_file>] [--gallery <gallery_name>] file1 [file2] [file3...]", add_help=False)
+parser.add_argument('--help', action='store_true')
+parser.add_argument('--log', default=None, type=argparse.FileType('a'))
+parser.add_argument('--tlog', default=None, type=argparse.FileType('a'))
+parser.add_argument('--gallery', default="Default") #this sets the gallery. It may fall over if you renamed your default gallery and you don't tell it the new name.
+parser.add_argument('files', nargs='*') #all other arguments treated as file names  #nargs=argparse.REMAINDER)
+
+args = parser.parse_args()
+
+#if you did --help, print the help message
+if args.help:
+	print_help()
+	exit()
+
+#set up logging
+def log(what):
+	if args.log is not None:
+		args.log.write(what)
+	if args.tlog is not None:
+		args.tlog.write(what)
+		
+if len(args.files) == 0: #specified no files? complain
+	print_usage()
+	print("Error: You need to specify at least one file to upload.")
+	exit()	
+
+#iterate over files, (try to) upload each
+for file in args.files:
 	try:
+		#check that we haven't been fed a directory by accident
+		if isdir(file):
+			print("Ignoring directory {} - Megasend doesn't upload recursively.".format(file))
+			continue #move on
+			
+		if not isfile(file):
+			print("File {} not found.".format(file))
+			log("Couldn't find file {}.\n".format(file))
+			continue #move on	
+			
 		with open(file, 'rb') as thisfile:
 			print("Uploading file {}.".format(file))
+			
+			#size check
 			size = getsize(file)
 			if size > 1024 * 1024 * my_mb_limit:
 				print("This file is too big to upload (over {} MB).".format(my_mb_limit))
+				log("Skipped overlarge file {}.\n".format(file))
 				continue #skip file
 		
-			r = requests.post("https://lpix.org/api", data={'username':username, 'password':password, 'gallery':gallery, 'output':'json'}, files={'file': thisfile})
-		
+			r = requests.post("https://lpix.org/api", data={'username':username, 'password':password, 'gallery':args.gallery, 'output':'json'}, files={'file': thisfile}) #actually upload file
 			result = r.json()
-			if result['err'] is not None:
+			
+			if result['err'] is not None: #check for fails, tell people
 				error = result['err']
 				permanent_error = False
 				
@@ -39,7 +85,7 @@ for file in argv[1:]:
 					error_explanation = 'authentication error. Check your username and password!'
 					permanent_error = True
 				elif error == 'err3':
-					error_explanation = 'this file doesn\'t appear to be an image or MP3 file.'
+					error_explanation = "this file doesn't appear to be an image or MP3 file."
 				elif error == 'err4':
 					error_explanation = 'your file is too big.'
 				elif error == 'err5':
@@ -51,10 +97,20 @@ for file in argv[1:]:
 					error_explanation = 'unknown error, using the error code "{}".'.format(error)
 					permanent_error = True
 				print("Received error when uploading {}: {}")
+				log("Couldn't upload file {}.\n".format(file))
 				if permanent_error: 
 					print("Stopping now.")
-					break # we're done uploading images
-				continue #otherwise, if there's an error, jump to the next file
+					break # abort! we're done uploading images
+				continue #otherwise, if there's an error, jump to the next file	
+				
+			#report success	
 			print("Image URL: [img]{}[/img]\nThumbnail code: [timg]{}[/timg]\n".format(result['imageurl'],result['thumburl']))
-	except IOError:
+			#log it (custom text)
+			if args.log is not None:
+				args.log.write('[img]{}[/img]\n'.format(result['imageurl']))
+			if args.tlog is not None:
+				args.tlog.write('[timg]{}[/timg]\n'.format(result['thumburl']))
+			
+	except IOError: #file not found? any sort of file access error
 		print("Couldn't open file {} for uploading.".format(file))
+		log("Couldn't open file {}.\n".format(file))
